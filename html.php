f882d683bb7d193ef454bd8fbfd70dfd210c7589
Adam Barański (Uczeń) <abaranski138@technikumkreatywne.pl>
2022-09-12 12:43:05 +0200

    PHP

---
+++ html.php
@@ -0,0 +1,35 @@
+<html>
+
+
+<head>
+
+
+
+
+</head>
+
+
+<body>
+<h1>yo</h1>
+<div></div>
+
+
+<?php
+    
+	$mycounter =1;
+	
+	echo "Hello world<br>";
+	
+		echo "jasfasdofjhnadsfh<br>";
+	
+	echo "My counter".$mycounter;
+?>
+
+
+
+
+
+</body>
+
+
+</html>
