9518338a4f1dcf6fa45eeeaa088bc129fa3d767a
AdamBaranski2 <75801141+AdamBaranski2@users.noreply.github.com>
2022-09-14 11:44:45 +0200

    Add files via upload

---
+++ zmienne.php
@@ -0,0 +1,42 @@
+<h1><?php
+
+
+    
+            $zmienna = 7;
+/*
+            echo "masz $zmienna wiadomości";
+*/
+
+if($zmienna==0)
+    {
+        echo "brak wiadomosci";
+
+    }
+    else
+    {
+
+        echo "masz $zmienna wiadomości";
+
+    }
+    $x=10;
+
+    $y=0;
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</h1>
