211a52121d12c3e5d2ec5b7919669c62855192ef
AdamBaranski2 <75801141+AdamBaranski2@users.noreply.github.com>
2022-09-21 11:49:13 +0200

    Add files via upload

---
+++ if oceny.php
@@ -0,0 +1,83 @@
+<br>
+<?php
+/*$ocena =34;
+
+
+
+
+
+
+if($ocena<=39)
+{
+echo"masz 1";
+}
+
+else if($ocena>=40 && $ocena <=54)
+{
+echo"masz 2";
+}
+
+else if($ocena>=55 && $ocena <=69)
+{
+echo"masz 3";
+}
+
+
+else if($ocena>=70 && $ocena <=84)
+{
+echo"masz 4";
+}
+
+
+else if($ocena>=40 && $ocena <=54)
+{
+echo"masz 5";
+}
+*/
+
+
+
+$x=-3;
+
+for($x=-3;$x<=3;$x++)
+{
+
+    if($x==0) continue;
+
+echo $x ."<br>";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
